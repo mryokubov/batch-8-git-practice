2deafdb78e06124625314e34e87b45886b3f53f8
TairovichJR <yokubov.omonjon@gmail.com>
2023-05-03 19:50:10 -0400

    implemenetd login page page object class

--- src/main/java/com/academy/techcenture/pages/LoginPage.java
+++ src/main/java/com/academy/techcenture/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package com.academy.techcenture.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "username")
+    protected WebElement usernameInput;
+
+
+    @FindBy(id = "password")
+    protected WebElement passwordInput;
+
+    @FindBy(id = "loginbtn")
+    protected WebElement loginBtn;
+
+    public void login(){
+        usernameInput.sendKeys("username");
+        passwordInput.sendKeys("password");
+        loginBtn.click();
+    }
 }
