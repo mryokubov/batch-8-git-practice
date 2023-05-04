3f2fe5ac23db3053e5782635ab1f6b7cf8b55a20
mryokubov <97797362+mryokubov@users.noreply.github.com>
2023-05-03 20:00:14 -0400

    updated order page class with the main method

--- src/main/java/com/academy/techcenture/pages/OrderPage.java
+++ src/main/java/com/academy/techcenture/pages/OrderPage.java
@@ -1,4 +1,16 @@
 package com.academy.techcenture.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class OrderPage {
+  
+  private WebDriver driver;
+  
+  public OrderPage(WebDriver driver){
+     this.driver = driver;
+  }
+  
+  public static void main(String[] args){
+    System.out.println("These changes were made on a different system");
+  }
 }
