fae6020191ba5b4a39f3129c0c1166732e5bbeb6
mryokubov <97797362+mryokubov@users.noreply.github.com>
2023-05-03 20:10:14 -0400

    working on place order automation

--- src/main/java/com/academy/techcenture/pages/OrderPage.java
+++ src/main/java/com/academy/techcenture/pages/OrderPage.java
@@ -13,4 +13,8 @@ public class OrderPage {
   public static void main(String[] args){
     System.out.println("These changes were made on a different system");
   }
+  
+  public void placeOrder(){
+    System.out.println("I am still working on this. Please give me 3 months");
+  }
 }
