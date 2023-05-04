fa7f4346f9235d160453967324675a41af7f272d
muharremkulak <kulakmuharrem@gmail.com>
2023-05-03 20:35:54 -0400

    Message is created by Muharrem Kulak

---
+++ src/main/java/com/academy/techcenture/pages/CheckoutPage.java
@@ -0,0 +1,18 @@
+package com.academy.techcenture.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPage {
+
+    public CheckoutPage (WebDriver driver){
+
+    }
+
+
+    @FindBy (id = "username")
+    protected WebElement usernameInput;
+
+
+}
