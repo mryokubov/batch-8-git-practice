29eacbb725b291020c8ab690b1f7d4abfa92f69f
Olesya Sitdikova <olesya53@Olesyas-MacBook-Pro.local>
2023-05-03 20:43:28 -0400

    committing driver and utils classes

---
+++ src/main/java/com/academy/techcenture/driver/Driver.java
@@ -0,0 +1,8 @@
+package com.academy.techcenture.driver;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        System.out.println("this is driver class");
+    }
+}
---
+++ src/main/java/com/academy/techcenture/driver/Utils.java
@@ -0,0 +1,8 @@
+package com.academy.techcenture.driver;
+
+public class Utils {
+    public static void main(String[] args) {
+
+        System.out.println("hello this is utils");
+    }
+}
