4ef7a617ecf7b3c5cd80ae602b07ad68b8ee545e
TairovichJR <yokubov.omonjon@gmail.com>
2023-05-29 21:23:03 -0400

    Implemented ConfigReader, closes #1

--- src/main/java/com/academy/techcenture/config/ConfigReader.java
+++ src/main/java/com/academy/techcenture/config/ConfigReader.java
@@ -2,6 +2,8 @@ package com.academy.techcenture.config;
 
 public class ConfigReader {
     public static void main(String[] args) {
-
+        System.out.println(
+                "hello"
+        );
     }
 }
